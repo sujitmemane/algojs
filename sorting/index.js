c02d1f800e3a5f3e7dbdcba0e7f73988d8bcf3d4
sujitmemane-auragold <sujit@auragold.in>
2026-07-30 09:04:01 +0530

    Implement bubble sort algorithm and add test case

--- sorting/index.js
+++ sorting/index.js
@@ -1,3 +1,33 @@
 // Bubble Sort
+const nums = [5,2,4,1]
+
+
+/*
+Time complexity of bubble sort will be
+O(N^2)
+*/
+
+function bubbleSort(nums){
+     const n = nums.length
+     for(let i=0;i<n-1;i++){
+        let swapped=false
+        for(let j=0;j<n-i-1;j++){
+            if(nums[j]>nums[j+1]){
+                [nums[j],nums[j+1]] =[nums[j+1],nums[j]]
+                swapped=true
+            }
+
+        }
+        if(!swapped)break
+     }
+
+    return nums
+
+}
+
+
+function selectionSort(nums){
+
+}
 
 

c88f42f187af1de69eb7caed9c48f7101f8ff11e
sujitmemane <officialsujitmemane@gmail.com>
2026-08-08 16:52:44 +0530

    Add Floyd's cycle detection algorithm to LinkedList class

--- linked-list/questions.js
+++ linked-list/questions.js
@@ -1,58 +1,75 @@
 console.log("---------LINKEDLIST-QUESTIONS------------");
 
 class ListNode {
-	constructor(val) {
-		this.val = val;
-		this.next = null;
-	}
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
 }
 
 class LinkedList {
-	constructor() {
-		this.head = null;
-	}
+    constructor() {
+        this.head = null;
+    }
 
-	append(val) {
-		const node = new ListNode(val);
+    append(val) {
+        const node = new ListNode(val);
 
-		if (!this.head) {
-			this.head = node;
-			return node;
-		}
+        if (!this.head) {
+            this.head = node;
+            return node;
+        }
 
-		let cur = this.head;
+        let cur = this.head;
 
-		while (cur.next) {
-			cur = cur.next;
-		}
+        while (cur.next) {
+            cur = cur.next;
+        }
 
-		cur.next = node;
+        cur.next = node;
 
-		return node;
-	}
+        return node;
+    }
 
-	print() {
-		let cur = this.head;
-		const out = [];
+    print() {
+        let cur = this.head;
+        const out = [];
 
-		while (cur) {
-			out.push(cur.val);
-			cur = cur.next;
-		}
+        while (cur) {
+            out.push(cur.val);
+            cur = cur.next;
+        }
 
-		console.log(out.join(" -> "));
-	}
+        console.log(out.join(" -> "));
+    }
+    /*
 
-    isCycleInLinkedListApproachOne(){
-        const hash =  new Set()
+
+
+    */
+    isCycleInLinkedListApproachOne() {
+        const hash = new Set()
         let current = this.head
-        while(current!==null){
-            if(hash.has(current.value)) return true
+        while (current !== null) {
+            if (hash.has(current.value)) return true
             hash.add(current)
             current = current.next
         }
         return false
     }
+
+
+    isCycleInLinkedListFloydAlgo() {
+        let slow = this.head
+        let fast = this.head
+        while (fast && fast.next) {
+            if (slow === fast) return true
+            slow = slow.next
+            fast = fast.next.next
+        }
+        return false
+    }
+
 }
 
 // Demo
